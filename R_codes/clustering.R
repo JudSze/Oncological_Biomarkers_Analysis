aa5b63a1a4690c46d31fb79a63d7964021e8bc38
Judit <szenei.judit@gmail.com>
2021-11-29 16:35:09 +0100

    testing cluster number determining methods

--- R_codes/clustering.R
+++ R_codes/clustering.R
@@ -4,7 +4,14 @@ if (!requireNamespace("BiocManager", quietly = TRUE))
 BiocManager::install("BiocManager")
 BiocManager::install("multiClust")
 
+install.packages("factoextra")
+install.packages("NbClust")
+install.packages("cluster")
+
+library(factoextra)
+library(NbClust)
 library(multiClust)
+library(cluster)
 
 #C:\Program Files\R\R-4.1.2
 
@@ -37,4 +44,17 @@ hclust_analysis <- cluster_analysis(sel.exp=ranked.exprs,  #output of probe_rank
 
 head(hclust_analysis)
 #C:\Users\Judit\Downloads\TreeView-1.2.0-win (2)\TreeView-1.2.0-win
- 
+
+#Elbow method
+fviz_nbclust(ann_data, kmeans, method = "wss", k.max=24) +
+    geom_vline(xintercept=1, linetype = 2)+
+  labs(subtitle = "Elbow method") #suggested 2
+
+#Silhouette method
+fviz_nbclust(ann_data, kmeans, method="silhouette")+
+labs(subtitle="Silhouette method") #suggested 2
+
+#Gap method
+ann_data
+gap_stat <- clusGap(ann_data, FUN = kmeans, nstart = 30, K.max = 24, B = 50)
+fviz_gap_stat(gap_stat) + theme_minimal() + ggtitle("fviz_gap_stat: Gap Statistic")
