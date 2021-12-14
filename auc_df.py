6299ec679ba310025396e9ebb8ff68d0e6564620
Judit <szenei.judit@gmail.com>
2021-12-14 18:37:25 +0100

    added 8 and 7

--- auc_df.py
+++ auc_df.py
@@ -1,5 +1,21 @@
 import pandas as pd
 
+#Looking at cluster group 8
+df_8=pd.read_csv("C:/Egyetem/Szakmai_gyak/Data_tables/clustered_r.csv", sep=";")
+df_8.head(5)
+df_8.loc[df_8.ClustGroup == 8, "Status"]="1"
+df_8.loc[df_8.ClustGroup != 8 , "Status"]= "0"
+
+df_8.to_csv("C:/Egyetem/Szakmai_gyak/Data_tables/df_6.csv", sep=";")
+
+#Looking at cluster group 7
+df_7=pd.read_csv("C:/Egyetem/Szakmai_gyak/Data_tables/clustered_r.csv", sep=";")
+df_7.head(5)
+df_7.loc[df_7.ClustGroup == 7, "Status"]="1"
+df_7.loc[df_7.ClustGroup != 7 , "Status"]= "0"
+
+df_7.to_csv("C:/Egyetem/Szakmai_gyak/Data_tables/df_6.csv", sep=";")
+
 """Changing status values"""
 #Looking at cluster group 6
 df_6=pd.read_csv("C:/Egyetem/Szakmai_gyak/Data_tables/clustered_r.csv", sep=";")
@@ -15,7 +31,7 @@ df_5.head(5)
 df_5.loc[df_6.ClustGroup == 5, "Status"]="1"
 df_5.loc[df_6.ClustGroup != 5 , "Status"]= "0"
 
-df_5.to_csv("C:/Egyetem/Szakmai_gyak/Data_tables/df_5.csv", sep=";")
+df_5.to_csv("C:/Egyetem/Szakmai_gyak/Data_tables/variance_analysis/df_5.csv", sep=";")
 
 #Looking at cluster group 4
 df_4=pd.read_csv("C:/Egyetem/Szakmai_gyak/Data_tables/clustered_r.csv", sep=";")
@@ -23,7 +39,7 @@ df_4.head(5)
 df_4.loc[df_4.ClustGroup == 4, "Status"]="1"
 df_4.loc[df_4.ClustGroup != 4 , "Status"]= "0"
 
-df_4.to_csv("C:/Egyetem/Szakmai_gyak/Data_tables/df_4.csv", sep=";")
+df_4.to_csv("C:/Egyetem/Szakmai_gyak/Data_tables/variance_analysis/df_4.csv", sep=";")
 
 #Looking at cluster group 3
 df_3=pd.read_csv("C:/Egyetem/Szakmai_gyak/Data_tables/clustered_r.csv", sep=";")
@@ -31,7 +47,7 @@ df_3.head(5)
 df_3.loc[df_3.ClustGroup == 3, "Status"]="1"
 df_3.loc[df_3.ClustGroup != 3 , "Status"]= "0"
 
-df_3.to_csv("C:/Egyetem/Szakmai_gyak/Data_tables/df_3.csv", sep=";")
+df_3.to_csv("C:/Egyetem/Szakmai_gyak/Data_tables/variance_analysis/df_3.csv", sep=";")
 
 #Looking at cluster group 2
 df_2=pd.read_csv("C:/Egyetem/Szakmai_gyak/Data_tables/clustered_r.csv", sep=";")
@@ -39,7 +55,7 @@ df_2.head(5)
 df_2.loc[df_2.ClustGroup == 2, "Status"]="1"
 df_2.loc[df_2.ClustGroup != 2 , "Status"]= "0"
 
-df_2.to_csv("C:/Egyetem/Szakmai_gyak/Data_tables/df_2.csv", sep=";")
+df_2.to_csv("C:/Egyetem/Szakmai_gyak/Data_tables/variance_analysis/df_2.csv", sep=";")
 
 #Looking at cluster group 1
 df_1=pd.read_csv("C:/Egyetem/Szakmai_gyak/Data_tables/clustered_r.csv", sep=";")
@@ -47,4 +63,4 @@ df_1.head(5)
 df_1.loc[df_1.ClustGroup == 1, "Status"]="1"
 df_1.loc[df_1.ClustGroup != 1 , "Status"]= "0"
 
-df_1.to_csv("C:/Egyetem/Szakmai_gyak/Data_tables/df_1.csv", sep=";")
+df_1.to_csv("C:/Egyetem/Szakmai_gyak/Data_tables/variance_analysis/df_1.csv", sep=";")
