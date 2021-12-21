5a7666d14f109422ada00b65adcafb4f190aa72b
Judit <szenei.judit@gmail.com>
2021-12-21 23:41:16 +0100

    minor code change based on path and dataframe

--- R_codes/ENSEMBLE.R
+++ R_codes/ENSEMBLE.R
@@ -4,7 +4,7 @@ BiocManager::install("clusterProfiler")
 
 library(clusterProfiler)
 #ez tartalmazza mondjuk az x darab szignifikás gén ENSEMBLE ID-t
-ranked <- read_excel("C:/Users/fekete.janos.admin/Desktop/ranked.xlsx")
+ranked <- read.table("C:/Egyetem/Szakmai_gyak/Data_tables/ontology/df_8_ann_ensembl.csv")
 # karakter vektorrá alakítás
 de<-as.list.Vector(ranked)
 ed<-unlist(de)
@@ -17,9 +17,10 @@ ego_CC <- enrichGO(gene         = edd,
                  pAdjustMethod = "BH",
                  pvalueCutoff  = 0.01,
                  qvalueCutoff  = 0.05)
-head(ego2, 10)
+head(ego_CC, 10)
 CC<-head(ego_CC, 10)
-write.table(CC, "CC.txt", append = TRUE, row.names = TRUE, col.name=TRUE)
+write.table(CC, "df8_CC.txt", append = TRUE, row.names = TRUE, col.name=TRUE)
+getwd()
 
 ego_MF <- enrichGO(gene         = edd,
                  OrgDb         = org.Hs.eg.db,
@@ -29,7 +30,7 @@ ego_MF <- enrichGO(gene         = edd,
                  pvalueCutoff  = 0.01,
                  qvalueCutoff  = 0.05)
 MF<-head(ego_MF, 10)
-write.table(MF, "MF.txt", append = TRUE, row.names = TRUE, col.name=TRUE)
+write.table(MF, "df8_MF.txt", append = TRUE, row.names = TRUE, col.name=TRUE)
 
 ego_BP <- enrichGO(gene         = edd,
                    OrgDb         = org.Hs.eg.db,
@@ -39,9 +40,10 @@ ego_BP <- enrichGO(gene         = edd,
                    pvalueCutoff  = 0.01,
                    qvalueCutoff  = 0.05)
 BP<-head(ego_BP, 10)
-write.table(BP, "BP.txt", append = TRUE, row.names = TRUE, col.name=TRUE)
+write.table(BP, "df8_BP.txt", append = TRUE, row.names = TRUE, col.name=TRUE)
 
 #bárplottok készítése
 barplot(ego_MF, drop = TRUE, showCategory = 10)
 barplot(ego_CC, drop = TRUE, showCategory = 10)
 barplot(ego_BP, drop = TRUE, showCategory = 10)
+
