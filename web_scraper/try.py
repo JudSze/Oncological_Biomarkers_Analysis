241539d20ec6bd996315626e743c90eaa7638449
Judit <szenei.judit@gmail.com>
2022-01-18 14:31:04 +0100

    finally creating a normal df (missing files due html issues)

--- web_scraper/try.py
+++ web_scraper/try.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup as bs
 import requests
 import regex as re
 import wget
+import pandas as pd
+from pathlib import Path
 
 global_list=[]
 
@@ -22,8 +24,7 @@ def get_KEGG_links(url, x):
         get_KEGG_links(link, 'text')
     
     
-get_KEGG_links('https://www.gsea-msigdb.org/gsea/msigdb/genesets.jsp?collection=CP:KEGG', 'KEGG')
-
+get_KEGG_links('https://www.gsea-msigdb.org/gsea/msigdb/genesets.jsp?collection=CP:KEGG', 'TRANSPORTERS')
 print(global_list)
 
 for url in global_list:
@@ -31,3 +32,17 @@ for url in global_list:
     name=str(url).replace("https://www.gsea-msigdb.org/gsea/msigdb/download_geneset.jsp?geneSetName=","").replace("=txt",".txt")
     path="C:/Egyetem/Szakmai_gyak/KEGG_pathways/"+name
     wget.download(text_url, path)
+    
+len(global_list)
+
+#creating database out of all that information
+root_dir = pathlib.Path('C:/Egyetem/Szakmai_gyak/KEGG_pathways/')
+data = {}
+
+for pth in root_dir.glob('*.txt'):
+    data[pth.stem] = pd.read_table(pth)
+
+df = pd.concat(data, axis=1)
+df
+df.to_csv('C:/Egyetem/Szakmai_gyak/KEGG_pathways/KEGG_df.csv')
+
